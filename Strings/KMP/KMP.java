2ff1f8aa8f837616212d753ea1b19bba37dd2dd8
Simran1196 <33983968+Simran1196@users.noreply.github.com>
2020-12-26 21:18:15 +0530

    Update and rename README.md to Strings/KMP/KMP.java

---
+++ Strings/KMP/KMP.java
@@ -0,0 +1,78 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class KMP
+
+ {
+	public static void main (String[] args)throws IOException
+        {
+            Scanner s  = new Scanner(System.in);
+             String txt = s.next();//"ABABDABACDABABCABAB"; 
+            String pat = s.next(); //"ABABACA"; 
+            new Test().KMPSearch(pat, txt); 
+        }
+        
+        static void KMPSearch(String pat, String txt)
+        {
+            int m = pat.length();
+            int n = txt.length();
+            
+            int[] longestprefixsuffix = new int[m];
+            computeLPS(longestprefixsuffix,pat,m);
+            
+            int i=0;
+            int j=0;
+            while(i<n && j<m)
+            {
+                if(pat.charAt(i)==pat.charAt(j))
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    if(j==0)
+                        i++;
+                    else
+                        j = longestprefixsuffix[j-1];
+                }
+            }
+            
+            if(j==m)
+                System.out.println("Match");
+            else
+               System.out.println("NonMatch."); 
+        }
+        
+
+        private static void computeLPS(int[] lps, String pat, int m) 
+        {
+            int i=0,j=1;
+            lps[0] = 0;
+            
+            
+            while(i<m && j<m )
+            {
+                if(pat.charAt(i)==pat.charAt(j))
+                {
+                    lps[j] = i+1;
+                    i++;
+                    j++;
+                }
+                else if(i==0)
+               
+                    lps[j++]=0;
+                else 
+                {
+                  i = lps[i-1];
+                }
+            }
+        }
+        
+}
+
+
+
+
